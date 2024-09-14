dc885a2bbc1fcc10dd4421c2d755cd0ee6c24068
Bhavesh Patil <111061723+bhaveshpatil07@users.noreply.github.com>
2024-09-14 15:56:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/e-plantShopping/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+  },
 })
